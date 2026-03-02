bcd83a666e304e88ffc3468d9327c49013b9ebe3
Pierre Deldalle <pierredeldallepro@gmail.com>
2026-03-02 09:25:48 +0100

    début de la mise en place des affiches

--- src/Pages/PagesPortfolio/visuelsaffiches.jsx
+++ src/Pages/PagesPortfolio/visuelsaffiches.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AffichageVisuels from "../../Composants/AffichageVisuels/AffichageVisuels";
+import Affiche1 from "../../Assets/Affiches/Affiche1.png";
 
 const visuelsaffiches = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AffichageVisuels nom="test" date="1 mars 2026" image={Affiche1} />
+    </div>
+  );
 };
 
 export default visuelsaffiches;
