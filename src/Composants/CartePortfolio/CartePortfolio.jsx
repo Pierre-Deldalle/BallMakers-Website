a9da38297f6d3dcf9863dc2102c72c58e3658eb8
Pierre Deldalle <pierredeldallepro@gmail.com>
2026-05-10 00:18:45 +0200

    Fix : Nouvelles images d'afifches encore moins lourdes que les précédentes, mise à jour et correction du code pour optimiser le site web.

--- src/Composants/CartePortfolio/CartePortfolio.jsx
+++ src/Composants/CartePortfolio/CartePortfolio.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 const CartePortfolio = ({ link, image, titre, date }) => {
   return (
     <Link to={link} className={style.CartePortfolio}>
-      <img src={image} alt={titre} className={style.imageContainer} />
+      <div className={style.imageWrapper}>
+        <img src={image} alt={titre} />
+      </div>
+
       <div className={style.textes}>
         <p className={style.texteTitre}>{titre}</p>
         <p className={style.texteDate}>{date}</p>
